6d4d2befd2ac6ca29bbcc31a838553b6dd815c57
Domhuynh <dominique.huynh@m4x.org>
2019-08-17 17:13:16 +0200

    Ajout d'une ligne pour tester le suivi de modification

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.print("Hello Dominique");
+        System.out.print("Hello Dominique.");
+        System.out.print("Modifié pour exercice sur GitHub");
     }
 }
